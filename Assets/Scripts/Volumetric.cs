6d8b965ac8a5d3cebaa83b2e1c7c327ba128c7b5
ZERATUL-PC\Zeratul <p.f.zeratul@gmail.com>
2020-09-13 00:50:34 +0800

    Finished world space viewDir

---
+++ Assets/Scripts/Volumetric.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(VolumetricRenderer), PostProcessEvent.AfterStack, "Custom/VolumetricRenderer")]
+public sealed class Volumetric : PostProcessEffectSettings
+{
+    //[Range(0f, 1f), Tooltip("Grayscale effect intensity.")]
+    //public FloatParameter blend = new FloatParameter { value = 0.5f };
+}
+
+public class VolumetricRenderer : PostProcessEffectRenderer<Volumetric>
+{
+    private Camera camera;
+    private CommandBuffer commandBuffer;
+    private Shader shader;
+    private Vector3[] frustumCorners = new Vector3[4];
+    private Vector4[] screenTriangleCorners = new Vector4[3];
+
+    public override void Init()
+    {
+        commandBuffer = new CommandBuffer()
+        {
+            name = "Volumetric Command Buffer"
+        };
+        shader = Shader.Find("Volumetric/VolumetricRenderer");
+    }
+
+    public override void Render(PostProcessRenderContext context)
+    {
+        camera = context.camera;
+        PropertySheet sheet = context.propertySheets.Get(shader);
+
+        camera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), camera.farClipPlane, camera.stereoActiveEye, frustumCorners);
+
+        screenTriangleCorners[0] = new Vector4(frustumCorners[1].x, frustumCorners[1].y, camera.farClipPlane, 0);
+        screenTriangleCorners[1] = new Vector4(frustumCorners[0].x, -3.0f * frustumCorners[1].y, camera.farClipPlane, 0);
+        screenTriangleCorners[2] = new Vector4(3.0f * frustumCorners[2].x, frustumCorners[1].y, camera.farClipPlane, 0);
+
+        screenTriangleCorners[0] = camera.transform.TransformVector(screenTriangleCorners[0]) / camera.farClipPlane;
+        screenTriangleCorners[1] = camera.transform.TransformVector(screenTriangleCorners[1]) / camera.farClipPlane;
+        screenTriangleCorners[2] = camera.transform.TransformVector(screenTriangleCorners[2]) / camera.farClipPlane;
+
+        sheet.properties.SetVectorArray("_ScreenQuadCorners", screenTriangleCorners);
+
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
---
+++ Assets/Shaders/VolumetricRenderer.shader
@@ -0,0 +1,62 @@
+﻿Shader "Volumetric/VolumetricRenderer"
+{
+    SubShader
+    {
+        Blend One Zero
+        Cull Off
+        ZTest Always
+        ZWrite Off
+
+        Pass
+        {
+            HLSLPROGRAM
+
+            #pragma vertex Vert
+            #pragma fragment Frag
+
+            #pragma enable_d3d11_debug_symbols
+
+            #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+            
+            struct Attributes
+            {
+                float3 vertex : POSITION;
+                uint id : SV_VertexID;
+            };
+
+            struct Varyings
+            {
+                float4 position : SV_POSITION;
+                float2 uv : TEXCOORD0;
+                float3 viewDir : TEXCOORD1;
+            };
+
+            float4 _ScreenQuadCorners[3];
+
+            Varyings Vert(Attributes v)
+            {
+                Varyings o;
+                o.position = float4(v.vertex.xy, 0.0, 1.0);
+                o.uv = TransformTriangleVertexToUV(v.vertex.xy);
+
+                #if UNITY_UV_STARTS_AT_TOP
+                    o.uv = o.uv * float2(1.0, -1.0) + float2(0.0, 1.0);
+                #endif
+
+                o.viewDir = _ScreenQuadCorners[v.id].xyz;
+
+                return o;
+            }
+
+            float4 Frag(Varyings IN) : SV_Target
+            {
+                float3 viewDirWS = IN.viewDir;
+
+                float4 color = float4(viewDirWS, 1);
+                //float4 color = float4(IN.uv, 0, 1);
+                return color;
+            }
+            ENDHLSL
+        }
+    }
+}
